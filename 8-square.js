b7cf7b283b999d227b08f912dcd44eb2688ca88f
SirLight <strokelight35@gmail.com>
2025-11-09 23:31:31 +0200

    sfedcxsw

--- 8-square.js
+++ 8-square.js
@@ -3,7 +3,7 @@ let arg1 = process.argv[2];
 if (!arg1) {
   console.log("Missing size");
 } else {
-  let size = parseInt(arg1, 10);
+  let size = parseInt(arg1);
   for (let i = 0; i < size; i++) {
     console.log("x".repeat(size));
   }
