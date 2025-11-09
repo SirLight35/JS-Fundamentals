b40a0703988985c175790b835899f8879dd50736
SirLight <strokelight35@gmail.com>
2025-11-09 23:28:10 +0200

    fdfd

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-let sar = ["C is fun", "Python is cool", "Javascript is amazing"];
+let sar = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 for (let i = 0; i < sar.length; i++) {
   console.log(sar[i]);
