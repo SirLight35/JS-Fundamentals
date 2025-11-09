dd445d6bff6d953e3774c30540e26bb5c00ca2e8
SirLight <strokelight35@gmail.com>
2025-11-09 23:25:30 +0200

    fsdf

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 let arg1 = process.argv[2];
 
 if (isNaN(arg1) || arg1.trim() === "") {
-  console.log("not a number");
+  console.log("Not a number");
 } else {
   console.log("My number: " + arg1);
 }
